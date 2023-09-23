87c02de3d45e2db44619e5f0b14342ed8c204bf6
githubirakli <118377638+githubirakli@users.noreply.github.com>
2023-09-23 23:27:14 +0400

    Update IntRepresentation.java

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-    }
+        char c='c';
+        char a='a';
+        char h='h';
+        char r='r';
+        int intC=(int) c;
+        int intA=(int) a;
+        int intH=(int) h;
+        int intR=(int) r;
+        System.out.println("Integer reprentesation of 'c':"+ intC);
+        System.out.println("Integer reprentesation of 'a':"+ intA);
+        System.out.println("Integer reprentesation of 'h':"+ intH);
+        System.out.println("Integer reprentesation of 'r':"+ intR);
+            }
 }
